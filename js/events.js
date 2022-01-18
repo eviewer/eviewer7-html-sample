d828d58f10146a9d69988fbdcdca6305d8c782b2
eViewer HTML5 Document Viewer <viewer@eviewer.net>
2022-01-18 15:24:42 +0530

    updated to support landing page number

--- js/events.js
+++ js/events.js
@@ -1,90 +1,135 @@
-function _docLoadComplete(docID) {
-	try {
-		console.log("docLoadComplete: " + docID);
-	} catch (exp) {
-	}
-}
+window._docLoadComplete = function (docID, time) {
+  try {
+    console.log(
+      "docLoadComplete for documentID: " + docID + " in " + time + "ms"
+    );
+  } catch (exp) {}
+};
 
-function _docSaveComplete(docID, annotationData) {
-	try {
-		console.log("docSaveComplete: " + docID + " annData: " + annotationData);
-	} catch (exp) {
-	}
-}
+window._firstPageRendered = function (docID, time) {
+  try {
+    console.log(
+      "firstPageRendered for documentID: " + docID + " in " + time + "ms"
+    );
+  } catch (exp) {}
+};
 
-function _docDropped(docID) {
-	try {
-		console.log("docDropped: " + docID);
-	} catch (exp) {
-	}
-}
+window._customButtonClicked = function (operation) {
+  try {
+    console.log("Mesage from react app: " + operation);
+  } catch (exp) {}
+};
 
-function _annCreated(docID, annID, pageNo) {
-	try {
-		console.log("annCreated: docID " + docID + " annID: " + annID + " pageNo: " + pageNo);
-	} catch (exp) {
-	}
-}
+window._docSaveComplete = function (docID, annotationData) {
+  try {
+    console.log("docSaveComplete: " + docID + " annData: " + annotationData);
+  } catch (exp) {}
+};
 
-function _annDeleted(docID, annID, pageNo) {
-	try {
-		console.log("annDeleted: docID " + docID + " annID: " + annID + " pageNo: " + pageNo);
-	} catch (exp) {
-	}
-}
+window._docDropped = function (docID) {
+  try {
+    console.log("docDropped: " + docID);
+  } catch (exp) {}
+};
 
-function _annPropUpdated(docID, pageNo, annID, annProperty) {
-	try {
-		console.log("annPropUpdated: docID " + docID + " annID: " + annID + " annProperty: " + annProperty);
-	} catch (exp) {
-	}
-}
+window._annCreated = function (docID, annID, pageNo) {
+  try {
+    console.log(
+      "annCreated: docID " + docID + " annID: " + annID + " pageNo: " + pageNo
+    );
+  } catch (exp) {}
+};
 
-function _buttonAnnClicked(docID, annID, pageNo, buttonText) {
-	try {
-		console.log("buttonAnnClicked: docID " + docID + " annID: " + annID + " pageNo: " + pageNo + " buttonText: " + buttonText);
-	} catch (exp) {
-	}
-}
+window._annDeleted = function (docID, annID, pageNo) {
+  try {
+    console.log(
+      "annDeleted: docID " + docID + " annID: " + annID + " pageNo: " + pageNo
+    );
+  } catch (exp) {}
+};
 
-function _pageChanged(docID, pageNo) {
-	try {
-		console.log("pageChanged: " + docID + " current pageNo: " + pageNo);
-	} catch (exp) {
-	}
-}
+window._annPropUpdated = function (docID, pageNo, annID, annProperty) {
+  try {
+    console.log(
+      "annPropUpdated: docID " +
+        docID +
+        " annID: " +
+        annID +
+        " annProperty: " +
+        annProperty
+    );
+  } catch (exp) {}
+};
 
-function _docExported(docID, selectedOption) {
-	try {
-		console.log("docExported: " + docID + " selectedOption: " + selectedOption);
-	} catch (exp) {
-	}
-}
+window._buttonAnnClicked = function (docID, annID, pageNo, buttonText) {
+  try {
+    console.log(
+      "buttonAnnClicked: docID " +
+        docID +
+        " annID: " +
+        annID +
+        " pageNo: " +
+        pageNo +
+        " buttonText: " +
+        buttonText
+    );
+  } catch (exp) {}
+};
 
-function _pageDeleted(docID, pageNo) {
-	try {
-		console.log("pageDeleted: " + docID + " current pageNo: " + pageNo);
-	} catch (exp) {
-	}
-}
+window._pageChanged = function (docID, pageNo) {
+  try {
+    console.log("pageChanged: " + docID + " current pageNo: " + pageNo);
+  } catch (exp) {}
+};
 
-function _pageCut(docID, pageNo) {
-	try {
-		console.log("pageCut: " + docID + " currnet pageNo: " + pageNo);
-	} catch (exp) {
-	}
-}
+window._docExported = function (docID, selectedOption) {
+  try {
+    console.log("docExported: " + docID + " selectedOption: " + selectedOption);
+  } catch (exp) {}
+};
 
-function _pageCopied(docID, pageNo) {
-	try {
-		console.log("pageCopied: " + docID + " current pageNo: " + pageNo);
-	} catch (exp) {
-	}
-}
+window._pageDeleted = function (docID, pageNo) {
+  try {
+    console.log("pageDeleted: " + docID + " current pageNo: " + pageNo);
+  } catch (exp) {}
+};
 
-function _pagePasted(docID, pageNo) {
-	try {
-		console.log("pagePasted: " + docID + " current pageNo: " + pageNo);
-	} catch (exp) {
-	}
-}
+window._pageCut = function (docID, pageNo) {
+  try {
+    console.log("pageCut: " + docID + " currnet pageNo: " + pageNo);
+  } catch (exp) {}
+};
+
+window._pageCopied = function (docID, pageNo) {
+  try {
+    console.log("pageCopied: " + docID + " current pageNo: " + pageNo);
+  } catch (exp) {}
+};
+
+window._pagePasted = function (docID, pageNo) {
+  try {
+    console.log("pagePasted: " + docID + " current pageNo: " + pageNo);
+  } catch (exp) {}
+};
+
+window._textSelected = function (response) {
+try {
+    console.log(response);
+
+    /*if (response !== undefined && response.coordinates !== undefined) {
+      let eViewerObj = new eViewerApp();
+      response.coordinates.forEach((element) => {
+        let pageRange = [];
+        pageRange.push(response.page);
+        let annotationData = {
+          X: element.x1,
+          Width: element.x2,
+          Y: element.y1,
+          Height: element.y3,
+        };
+        eViewerObj.annotationService.selectShape("highlight");
+        eViewerObj.annotationService.drawShapes(pageRange, annotationData);
+      });
+    }*/
+  } catch (exp) {}
+};
